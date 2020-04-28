3a285784e62c5dc7ef85b02422de8ed9d39edd2c
dilanrinconhbtn <937@holbertonschool.com>
2020-04-28 14:54:15 +0000

    fix position of index and segementation fault

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -7,23 +7,29 @@ listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *node, *tmp;
 
-	node = malloc(sizeof(dlistint_t));
+	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 	{
 		free(node);
 		return (NULL);
 	}
-	node->n = n;
+	node->n = number;
 	node->next = NULL;
 	if (*head == NULL)
 		*head = node;
 	else
 	{
 		tmp = *head;
-		while (tmp->next != NULL)
+		while (tmp->next == NULL)
+		{
+			if (tmp->next == NULL || tmp->next->n > node->n)
+                        {
+				node->next = tmp->next;
+				tmp->next = node;
+				break;
+			}
 			tmp = tmp->next;
-		tmp->next = node;
-		node->prev = tmp;
+		}
 	}
-	return (*head);
+	return (node);
 }
